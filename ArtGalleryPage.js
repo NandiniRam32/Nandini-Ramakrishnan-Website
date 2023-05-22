33dffeb8f8740661fa0f8104db40c4a99e401e8d
Nandini Ramakrishnan <126700503+NandiniRam32@users.noreply.github.com>
2023-05-22 09:32:58 -0700

    Add files via upload

--- ArtGalleryPage.js
+++ ArtGalleryPage.js
@@ -1,28 +1,21 @@
-let num_pic = 1;
-let num_left = 0;
-let num_right = 2;
+let num_left_pic = 0;
 let mainImageName, leftImageName, rightImageName;
 let generatePrevious = document.getElementById('previous-button');
 let generateNext =  document.getElementById('next-button');
 generatePrevious.addEventListener('click', () => {
-    num_pic = ((num_pic + 7) % 8);
-    num_left = ((num_left + 7) % 8);
-    num_right = ((num_right + 7) % 8);
-    mainImageName = 'Drawing' + num_pic + '.jpg';
-    leftImageName = 'Drawing' + num_left + '.jpg';
-    rightImageName = 'Drawing' + num_right + '.jpg';
-    document.getElementById('left-photo').innerHTML = `<img class="left-photo" src="${leftImageName}" alt="Sorry, Image Not Found">`;
-    document.getElementById('right-photo').innerHTML = `<img class="right-photo" src="${rightImageName}" alt="Sorry, Image Not Found">`;
-    document.getElementById('main-photo').innerHTML = `<img class="main-photo" src="${mainImageName}" alt="Sorry, Image Not Found">`;
+    num_left_pic = ((num_left_pic + 7) % 8);
+    updateImages();
 })
 generateNext.addEventListener('click', () => {
-    num_pic = ((num_pic + 1) % 8);
-    num_left = (num_left + 1) % 8;
-    num_right = (num_right + 1) % 8;
-    mainImageName = 'Drawing' + num_pic + '.jpg';
-    leftImageName = 'Drawing' + num_left + '.jpg';
-    rightImageName = 'Drawing' + num_right + '.jpg';
-    document.getElementById('left-photo').innerHTML = `<img class="left-photo" src="${leftImageName}" alt="Sorry, Image Not Found">`;
-    document.getElementById('right-photo').innerHTML = `<img class="right-photo" src="${rightImageName}" alt="Sorry, Image Not Found">`;
-    document.getElementById('main-photo').innerHTML = `<img class="main-photo" src="${mainImageName}" alt="Sorry, Image Not Found">`;
+    num_left_pic = ((num_left_pic + 9) % 8);
+    updateImages();
 })
+function updateImages() {
+    leftImageName = 'Drawing' + ((num_left_pic + 7) % 8) + '.jpg';
+    mainImageName = 'Drawing' + ((num_left_pic) % 8) + '.jpg';
+    rightImageName = 'Drawing' + ((num_left_pic + 1) % 8) + '.jpg';
+    //document.getElementById('left-photo').src = leftImageName;
+    document.getElementById('left-photo').innerHTML = `<img class="left-photo" src="${leftImageName}" alt="Sorry, Image Not Found">`;
+    document.getElementById('main-photo').innerHTML = `<img class="main-photo" src="${mainImageName}" alt="Sorry, Image Not Found">`;
+    document.getElementById('right-photo').innerHTML = `<img class="right-photo" src="${rightImageName}" alt="Sorry, Image Not Found">`;
+}
