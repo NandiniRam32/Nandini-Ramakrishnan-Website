96e551e0c34f571fdc81638715beb90a37e20a2c
Nandini Ramakrishnan <126700503+NandiniRam32@users.noreply.github.com>
2023-05-27 20:09:26 -1000

    Add new photographs

--- PhotoGalleryPage.js
+++ PhotoGalleryPage.js
@@ -3,17 +3,17 @@ let mainImageName, leftImageName, rightImageName;
 let generatePrevious = document.getElementById('previous-button');
 let generateNext =  document.getElementById('next-button');
 generatePrevious.addEventListener('click', () => {
-    num_left_pic = ((num_left_pic + 19) % 20);
+    num_left_pic = ((num_left_pic + 23) % 24);
     updateImages();
 })
 generateNext.addEventListener('click', () => {
-    num_left_pic = ((num_left_pic + 21) % 20);
+    num_left_pic = ((num_left_pic + 25) % 24);
     updateImages();
 })
 function updateImages() {
-    leftImageName = 'Photo' + ((num_left_pic + 19) % 20) + '.jpg';
-    mainImageName = 'Photo' + ((num_left_pic) % 20) + '.jpg';
-    rightImageName = 'Photo' + ((num_left_pic + 1) % 20) + '.jpg';
+    leftImageName = 'Photo' + ((num_left_pic + 23) % 24) + '.jpg';
+    mainImageName = 'Photo' + ((num_left_pic) % 24) + '.jpg';
+    rightImageName = 'Photo' + ((num_left_pic + 1) % 24) + '.jpg';
     document.getElementById('left-photo').src = leftImageName;
     document.getElementById('main-photo').src = mainImageName;
     document.getElementById('right-photo').src = rightImageName;
