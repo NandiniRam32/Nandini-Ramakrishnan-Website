467928f82c2acf9ef530fe082c3d0948e114caff
Nandini Ramakrishnan <126700503+NandiniRam32@users.noreply.github.com>
2023-05-31 08:20:31 -1000

    Bootstrap navbar
    Adding hover and dropdown features to bootstrap navbar

---
+++ navbar.js
@@ -0,0 +1,11 @@
+function toggleAboutMe() {
+    document.getElementById("myDropdownAboutMe").classList.toggle("show");
+}
+
+function toggleArtPage() {
+    document.getElementById("myDropdownArtPage").classList.toggle("show");
+}
+
+function togglePhotographyPage() {
+    document.getElementById("myDropdownPhotographyPage").classList.toggle("show");
+}
