42c2b95b09799670ba85113b90582bab49795751
Nandini Ramakrishnan <126700503+NandiniRam32@users.noreply.github.com>
2023-05-31 16:13:19 -1000

    Adding stickyness to the navbar

--- navbar.js
+++ navbar.js
@@ -9,3 +9,21 @@ function toggleArtPage() {
 function togglePhotographyPage() {
     document.getElementById("myDropdownPhotographyPage").classList.toggle("show");
 }
+
+window.onscroll = function() {
+    navbarStickyness()
+};
+
+var navbar = document.getElementById("navbar");
+var navbarSubsection = document.getElementById("navbarSubsection");
+var sticky = navbar.offsetTop;
+
+function navbarStickyness() {
+    navbar.classList.add("stickyMainNavbar");
+    if (window.pageYOffset <= sticky) {
+        navbarSubsection.classList.add("stickyNavbarSubsection");
+    } else {
+        navbarSubsection.classList.remove("stickyNavbarSubsection");
+        navbarSubsection.classList.add("stickyNavbarSubsectionGone");
+    }
+}
